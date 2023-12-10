2420f07739f066c605903b249718ab42ef162d13
purn25 <152841950+purn25@users.noreply.github.com>
2023-12-10 23:04:32 +0900

    Update deterministic_findT.py

--- deterministic_findT.py
+++ deterministic_findT.py
@@ -4,8 +4,7 @@ from scipy.integrate import ode
 import scipy.signal
 import math
 import matplotlib.pyplot as plt
-#param
-#set up simulator
+
 def f(t, y, arg):
     vin_0 = arg[0]
     ks = arg[1]
@@ -42,10 +41,9 @@ def main():
 
 	vin_0 = 0.005
 	ks = 0.05
-	#Tin = 200
-#	Tin = int(sys.argv[1])
 	c = 0.01
 	A0 = float(sys.argv[1])
+	Tin = int(sys.argv[2])
 	a = 2000
 	d = 100
 	L = 4000000000
@@ -53,58 +51,40 @@ def main():
 	Dzero = 0.0005
 	n = 8
 
-	interval = (10,2000)
-	#interval = (10,1000)
-	accuracy = 10
-	filename = open("/home/purn25/glycolysis_period_A%.4lf.dat"%A0,"w")
-
 
 	t1 = 100000
 	dt = .01
 	y0 =[0,0]
 	t0 = 0
-	TinArray = np.array([])
-	YSumArray = np.array([])
-	for Tin in np.arange(interval[0],interval[1],accuracy):
-	#	params = np.array([vin_0,ks,Tin,c,A0,a,d,L,kk,Dzero,n])
-		X = np.zeros( int(t1/dt)+1 )
-		Y = np.zeros( int(t1/dt)+1 )
-		T = np.zeros( int(t1/dt)+1 )
-		r = ode(f, jac).set_integrator('vode', method='bdf', with_jacobian=True)
-		r.set_initial_value(y0, t0).set_f_params([vin_0,ks,Tin,c,A0,a,d,L,kk,Dzero,n]).set_jac_params([vin_0,ks,Tin,c,A0,a,d,L,kk,Dzero,n])
-		i=0
-		while r.successful() and r.t < t1:
-			r.integrate(r.t+dt)
-			T[i] = r.t
-			X[i] = r.y[0]
-			Y[i] = r.y[1]
-			i=i+1
 
-		Time = []
-		for time in range(len(T)):	
-			if vin_0+A0*math.sin(2*math.pi*T[time]/Tin)<0:
-				Time.append(0)
-			else:
-				Time.append(vin_0+A0*math.sin(2*math.pi*T[time]/Tin))
-		Trange = Time[8000000:]
-		Xrange = X[8000000:]
-		Yrange = Y[8000000:]
-		PeriodList = []
-		for peak in range(len(Xrange)):
-			if round(Xrange[peak],4) == round(Xrange[0],4)and round(Yrange[peak],4) == round(Yrange[0],4):
-#			if Xrange[peak] == Xrange[0] and Yrange[peak] == Yrange[0] and Trange[peak] == Trange[0]:
-				PeriodList.append(peak)
-#		print(PeriodList)
-		Period = []
-		perioddiff = np.diff(PeriodList)
-		for peakdiff in range(len(perioddiff)):
-			if perioddiff[peakdiff] > 2:
-				Period.append(perioddiff[peakdiff])
-#		print(Period)
-		if Period !=[] and len(Period) > 1:
-			filename.write("%d\t%lf\t%lf\n"%(Tin,float(np.mean(Period)),float(np.std(Period))))
+	X = np.zeros( int(t1/dt)+1 )
+	Y = np.zeros( int(t1/dt)+1 )
+	T = np.zeros( int(t1/dt)+1 )
+	r = ode(f, jac).set_integrator('vode', method='bdf', with_jacobian=True)
+	r.set_initial_value(y0, t0).set_f_params([vin_0,ks,Tin,c,A0,a,d,L,kk,Dzero,n]).set_jac_params([vin_0,ks,Tin,c,A0,a,d,L,kk,Dzero,n])
+	i=0
+	while r.successful() and r.t < t1:
+		r.integrate(r.t+dt)
+		T[i] = r.t
+		X[i] = r.y[0]
+		Y[i] = r.y[1]
+		i=i+1
 
-	filename.close()
+
+	Xrange = X[8000000:]
+	Yrange = Y[8000000:]
+	PeriodList = []
+	for peak in range(len(Xrange)):
+		if round(Xrange[peak],5) == round(Xrange[0],5)and round(Yrange[peak],5) == round(Yrange[0],5):
+			PeriodList.append(peak)
+	Period = []
+	perioddiff = np.diff(PeriodList)
+	for peakdiff in range(len(perioddiff)):
+		if perioddiff[peakdiff] > 2:
+			Period.append(perioddiff[peakdiff])
+	if Period !=[] and len(Period) > 1:
+		print(Tin,float(np.mean(Period)),float(np.std(Period)))
+
 
 	return
 
